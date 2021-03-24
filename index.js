fcf5d2b9ec113685f9e6a33ef765f800cfa896da
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2021-03-24 09:02:37 -0700

    Updating index.js file

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const express = require('express')
-const subscripotion = require('./subscription')
-const app = express()
-const port = 80
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const helmet = require("helmet");
+const cors = require("cors");
+const http = require("http");
+const path = require("path");
+const WebSocket = require("ws");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const { MAIN_PORT, NODE_ENV, GOOGLE_APPLICATION_CREDENTIALS } = process.env;
+NODE_ENV !== "production"
+  ? app.use(morgan("dev"))
+  : app.use(morgan("combined"));
+const { listenForPullMessages } = require("./subscription");
+app.use(helmet());
+app.use(cors());
+app.use(['/static'],express.static(path.join(__dirname, 'static')));
 
-app.get('/', (req, res) => {
-  res.send('Jai Mahakal !')
-})
+app.get("/", (req, res) => {
+  return res.render(path.join(__dirname,'/static/index.ejs'));
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.all("*", (req, res) => {
+    res.status(404).send('Not found');
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        console.log(`Broadcasting data ${data}`);
+        client.send(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+};
+
+server.listen(MAIN_PORT, () => {
+  console.log(`Example app listening at http://localhost:${MAIN_PORT}`);
+});
+
+listenForPullMessages(wss); //Listening the incoming telemetry;
